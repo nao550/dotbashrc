9af7f57874979cf2ba00035b2ae51f7a71eb224a
IKENO Naoki <nao@kyo-to.net>
2026-08-27 13:10:30 +0900

    Install NVM bashrc module

--- install.sh
+++ install.sh
@@ -33,6 +33,7 @@ mkdir -p "$BASHRC_D"
 fetch_file "$BASE_URL/.bashrc" "$BASHRC"
 fetch_file "$BASE_URL/.bashrc.d/10-history.sh" "$BASHRC_D/10-history.sh"
 fetch_file "$BASE_URL/.bashrc.d/11-alias.sh" "$BASHRC_D/11-alias.sh"
+fetch_file "$BASE_URL/.bashrc.d/20-nvm.sh" "$BASHRC_D/20-nvm.sh"
 
 # 旧ファイル名が残っている場合は重複読み込みを避けるため削除する。
 rm -f "$BASHRC_D/10-alias.sh"
@@ -40,6 +41,7 @@ rm -f "$BASHRC_D/10-alias.sh"
 echo "installed: $BASHRC"
 echo "installed: $BASHRC_D/10-history.sh"
 echo "installed: $BASHRC_D/11-alias.sh"
+echo "installed: $BASHRC_D/20-nvm.sh"
 echo
 echo "現在の Bash に反映する場合:"
 echo "  source ~/.bashrc"
