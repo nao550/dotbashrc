bbfd649595dd6b789842149f37122752233c4f90
IKENO Naoki <nao@kyo-to.net>
2026-08-28 15:26:04 +0900

    Install vim-plug with dotfiles

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@
 set -eu
 
 BASE_URL="https://raw.githubusercontent.com/nao550/dotbashrc/main"
+VIM_PLUG_URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+
 BASHRC="$HOME/.bashrc"
 ZSHRC="$HOME/.zshrc"
 VIMRC="$HOME/.vimrc"
+VIM_AUTOLOAD="$HOME/.vim/autoload"
+VIM_PLUG="$VIM_AUTOLOAD/plug.vim"
 SHELLRC_D="$HOME/.shellrc.d"
 OLD_BASHRC_D="$HOME/.bashrc.d"
 
@@ -40,6 +44,7 @@ backup_file "$ZSHRC"
 backup_file "$VIMRC"
 
 mkdir -p "$SHELLRC_D"
+mkdir -p "$VIM_AUTOLOAD"
 
 fetch_file "$BASE_URL/.bashrc" "$BASHRC"
 fetch_file "$BASE_URL/.zshrc" "$ZSHRC"
@@ -50,6 +55,12 @@ fetch_file "$BASE_URL/.shellrc.d/11-alias.sh" "$SHELLRC_D/11-alias.sh"
 fetch_file "$BASE_URL/.shellrc.d/20-nvm.sh" "$SHELLRC_D/20-nvm.sh"
 fetch_file "$BASE_URL/.shellrc.d/60-bashrc.sh" "$SHELLRC_D/60-bashrc.sh"
 fetch_file "$BASE_URL/.shellrc.d/70-zshrc.sh" "$SHELLRC_D/70-zshrc.sh"
+
+# vim-plug 本体がない場合のみ導入する。
+if [ ! -s "$VIM_PLUG" ]; then
+    fetch_file "$VIM_PLUG_URL" "$VIM_PLUG"
+    echo "installed: $VIM_PLUG"
+fi
 
 # 旧 ~/.bashrc.d の本リポジトリ由来ファイルが残っている場合は削除する。
 rm -f \
@@ -65,6 +76,9 @@ echo "installed: $BASHRC"
 echo "installed: $ZSHRC"
 echo "installed: $VIMRC"
 echo "installed: $SHELLRC_D"
+echo
+echo "Vim プラグインを初回インストールする場合:"
+echo "  vim +PlugInstall +qall"
 echo
 echo "現在のシェルに反映する場合:"
 echo "  Bash: source ~/.bashrc"
